8bd9db6a920aea8440adf2b3442a54933cbd8590
sergiopani <xpanisergio@gmail.com>
2022-07-03 13:13:45 +0200

    Todo menos Array de producidos

--- scripts/index.js
+++ scripts/index.js
@@ -1,101 +1,111 @@
 //const { getProduct } = require("../../../controllers/product");
 
 const app = new Vue({
-  el: "#app",
-  data: {
-    //Array de articulos que parseamos desde el fichero json
-    articulos: [],
-    articulosProducir:[],
-    //Array del fetch de json de empresas
-    empresas: [],
+    el: "#app",
+    data: {
+        //Array de articulos que parseamos desde el fichero json
+        articulos: [],
+        articulosProducir: [],
+        //Array del fetch de json de empresas
+        empresas: [],
 
-    //Por defecto primero va a tener la primera empresa de la array
-    currentEmpresa: {},
-    currentENombre:"",
-    currentESerie:"",
+        //Por defecto primero va a tener la primera empresa de la array
+        currentEmpresa: {},
+        currentENombre: "",
+        currentESerie: "",
 
-    //Atributos de los aticulos
-    fondos:[],
-    laterales:[],
-    tapas:[],
+        //Atributos de los aticulos
+        fondos: [],
+        laterales: [],
+        tapas: [],
 
-  },
-  /*Metodos que se ejecutan al iniciar la pagina*/
-  created() {
-    this.getProducts();
-    this.getEmpresas();
-  },
-  methods: {
-    comprobar:function (index){
-      console.log(this.fondos[0])
-      if(this.fondos[index] === true && this.laterales[index] === true && this.tapas[index] === true){
-        return true;
-      }else if(this.fondos[index] === undefined && this.laterales[index] === true && this.tapas[index] === true){
-        return true;
-      }else if(this.fondos[index] === undefined && this.laterales[index] === undefined && this.tapas[index] === true){
-        return true;
-      }else if(this.fondos[index] === true && this.laterales[index] === undefined && this.tapas[index] === undefined){
-        return true;
-      }else if(this.fondos[index] === undefined && this.laterales[index] === true && this.tapas[index] === undefined){
-        return true;
-      }
     },
-    setSerieByName:function(name){
-      this.currentESerie = name;
+    /*Metodos que se ejecutan al iniciar la pagina*/
+    created() {
+        this.getProducts();
+        this.getEmpresas();
     },
-    filter:function(a){
+    methods: {
+        comprobar: function (index) {
+            console.log(this.fondos[index])
+            console.log(this.laterales[index])
+            console.log(this.tapas[index])
+            if (this.fondos[index] === true && this.laterales[index] === true && this.tapas[index] === true) {
+                return true;
+            } else if (this.fondos[index] === 'none' && this.laterales[index] === 'none' && this.tapas[index] === true) {
+                return true;
+            } else if (this.fondos[index] === true && this.laterales[index] === 'none' && this.tapas[index] === 'none') {
+                return true;
+            } else if (this.fondos[index] === 'none' && this.laterales[index] === true && this.tapas[index] === 'none') {
+                return true;
+            } else if (this.fondos[index] === true && this.laterales[index] === 'none' && this.tapas[index] === true) {
+                return true;
+            } else if (this.fondos[index] === 'none' && this.laterales[index] === true && this.tapas[index] === true) {
+                return true;
+            } else if (this.fondos[index] === true && this.laterales[index] === true && this.tapas[index] === 'none') {
+                return true;
+            }
 
-      if(a.serie.toUpperCase() === this.currentESerie.toUpperCase()){
-        return a;
-      }
-    },
+        },
+        sendProduction: function () {
 
-    setDefault:function (e){
-      this.currentEmpresa = e;
-      this.currentENombre = e.nomfiscli;
-      this.currentESerie = e.serie;
-    },
-    //Devolemos si esta checheado con una okChecked
-    /*
-    printStateF: function (a) {
-      if (a.fondo === "X") {
-        return this.showUnChecked();
+        },
+        setSerieByName: function (name) {
+            this.currentESerie = name;
+        },
+        filter: function (a) {
 
-      }
-    },
-    printStateL: function (a) {
-      if (a.lateral === "X") {
-        return this.showUnChecked();
-      }
-    },
-    printStateT: function (a) {
-      if (a.tapa === "X") {
-        return this.showUnChecked();
-      }
-    },
-    showUnChecked: () => {
-      return `<div class="form-check">
-            <input class="form-check-input"  type="checkbox" value="accepted" unchecked-value="not_accepted" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">            
-            </label>
-            </div>
-            `;
-    },
-    */
+            if (a.serie.toUpperCase() === this.currentESerie.toUpperCase()) {
+                return a;
+            }
+        },
 
-    getProducts() {
-      fetch("../data/result.json")
-        .then((res) => res.json())
-        .then((data) => ((this.articulos = data), console.log(this.articulos)))
-        .catch((err) => console.log(err.message));
+        setDefault: function (e) {
+            this.currentEmpresa = e;
+            this.currentENombre = e.nomfiscli;
+            this.currentESerie = e.serie;
+        },
+        //Devolemos si esta checheado con una okChecked
+        /*
+        printStateF: function (a) {
+          if (a.fondo === "X") {
+            return this.showUnChecked();
+
+          }
+        },
+        printStateL: function (a) {
+          if (a.lateral === "X") {
+            return this.showUnChecked();
+          }
+        },
+        printStateT: function (a) {
+          if (a.tapa === "X") {
+            return this.showUnChecked();
+          }
+        },
+        showUnChecked: () => {
+          return `<div class="form-check">
+                <input class="form-check-input"  type="checkbox" value="accepted" unchecked-value="not_accepted" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                </label>
+                </div>
+                `;
+        },
+        */
+
+        getProducts() {
+            fetch("../data/result.json")
+                .then((res) => res.json())
+                .then((data) => ((this.articulos = data), console.log(this.articulos)))
+                .catch((err) => console.log(err.message));
+        },
+        getEmpresas() {
+            fetch("../data/empresas.json")
+                .then((res) => res.json())
+                .then((data) => ((this.empresas = data), console.log(this.empresas)))
+                .catch((err) => console.log(err.message));
+        },
     },
-    getEmpresas() {
-      fetch("../data/empresas.json")
-        .then((res) => res.json())
-        .then((data) => ((this.empresas = data), console.log(this.empresas)))
-        .catch((err) => console.log(err.message));
-    },
-  },
 });
 
 
